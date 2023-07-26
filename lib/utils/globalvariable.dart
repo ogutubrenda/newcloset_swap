049ae44c133dbf7eabfd61d8e368662cc3de48e9
Yvonne022 <yvonne.ndambiri@strathmore.edu>
2023-07-26 07:37:57 +0300

    newone

--- lib/utils/globalvariable.dart
+++ lib/utils/globalvariable.dart
@@ -6,3 +6,7 @@ import 'package:flutter/material.dart';
 //import 'package:instagram_clone_flutter/screens/search_screen.dart';
 
 const webScreenSize = 600;
+List<Widget> homeScreenItems = [
+  
+  
+];
